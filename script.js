d4c8fc304a13d65b733832856522d8ae39eeae46
spilbertina <spilbertina@gmail.com>
2022-03-29 00:25:53 +0300

    script popup for container

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let popup = document.querySelector('.popup');
-let popupOpen = document.querySelector('.profile__edit');
+let popupOpen = document.querySelector('.profile__edit ');
 let popupClose = popup.querySelector('.popup__close');
 
 function popupToggleX() {
-    popup.classList.toggle('popup__open');
+    popup.classList.toggle('popup__display-none');
 }
+
+popup.addEventListener('click', function(event) {
+    if(event.target === event.currentTarget) {
+        popupToggleX();
+    }
+});
 
 popupOpen.addEventListener('click', popupToggleX);
 
