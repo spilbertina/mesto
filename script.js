553b2c2cfd28c3c08ddb0c0807956feb8ab18ae9
spilbertina <spilbertina@gmail.com>
2022-03-30 16:56:50 +0300

    added change in js function formSubmitHandler

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let popup = document.querySelector('.popup');
 let popupOpen = document.querySelector('.profile__edit ');
 let popupClose = popup.querySelector('.popup__close');
+let formElement = popup.querySelector('.popup__form-button');
+let nameInput = popup.querySelector('.popup__form-name');
+let jobInput = popup.querySelector('.popup__form-job');
+console.log(popup);
 
 function popupToggleX() {
     popup.classList.toggle('popup__display-none');
@@ -10,8 +14,23 @@ popup.addEventListener('click', function(event) {
     if(event.target === event.currentTarget) {
         popupToggleX();
     }
+    console.log('click!');
 });
 
 popupOpen.addEventListener('click', popupToggleX);
 
 popupClose.addEventListener('click', popupToggleX);
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    console.log(nameInput[0].value);
+    console.log(jobInput[0].value);
+    nameInput.className
+}
+
+popupClose.addEventListener('click', function(event) {
+    if(event.target === event.currentTarget) {
+        popupToggleX();
+    }
+    console.log('click!');
+});
