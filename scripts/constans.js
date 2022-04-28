320f1232cf0b40f19094c8f12a2fe1ec8c4c598e
spilbertina <spilbertina@gmail.com>
2022-04-28 16:35:00 +0300

    correction template in html + likeActive

--- scripts/constans.js
+++ scripts/constans.js
@@ -24,16 +24,3 @@ const CARDS = [
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
-
-function element(card) {
-    return `<div class="element">
-    <img class="element__image" src="${card.link}" alt="Карачаевск">
-        <button class="element__trash" aria-label="Мусорная корзина" type="button"></button>
-        <div class="element__info">
-            <h2 class="element__title">${card.name}</h2>
-            <button class="element__like" aria-label="Сердечко" type="button">
-            </button>
-        </div>
-</div>`;
-}
-
--- scripts/index.js
+++ scripts/index.js
@@ -14,11 +14,13 @@ let popupCardOpen = document.querySelector('.profile__button');
 let popupCardClose = popupCard.querySelector('.popup__close');
 let popupCardForm = popupCard.querySelector('.popup__form');
 
+
 let popupImage = document.querySelector('.popup_image');
 let popupImageClose = popupImage.querySelector('.popup__close');
 
-popupProfile.addEventListener('click', function(event) {
-    if(event.target === event.currentTarget) {
+//-------Закрытие и открытие попапа popupProfile по области на странице и кнопкам. Отправка формы
+popupProfile.addEventListener('click', function (event) {
+    if (event.target === event.currentTarget) {
         popupProfile.classList.toggle('popup_show');
     }
 });
@@ -31,11 +33,11 @@ popupProfileClose.addEventListener('click', () => {
     popupProfile.classList.toggle('popup_show');
 });
 
-popupProfileForm.addEventListener('submit', popupProfileFormSubmitHandler); 
+popupProfileForm.addEventListener('submit', popupProfileFormSubmitHandler);
 
-//---------
-popupCard.addEventListener('click', function(event) {
-    if(event.target === event.currentTarget) {
+//---------Закрытие и открытие попапа popupCard по области на странице и кнопкам. Отправка формы
+popupCard.addEventListener('click', function (event) {
+    if (event.target === event.currentTarget) {
         popupCard.classList.toggle('popup_show');
     }
 });
@@ -48,11 +50,11 @@ popupCardClose.addEventListener('click', () => {
     popupCard.classList.toggle('popup_show');
 });
 
-popupCardForm.addEventListener('submit', popupCardFormSubmitHandler); 
+popupCardForm.addEventListener('submit', popupCardFormSubmitHandler);
 
-//--------- 
-popupImage.addEventListener('click', function(event) {
-    if(event.target === event.currentTarget) {
+//---------Закрытие и открытие попапа popupImage по области на странице и кнопкам
+popupImage.addEventListener('click', function (event) {
+    if (event.target === event.currentTarget) {
         popupImage.classList.toggle('popup_show');
     }
 });
@@ -61,43 +63,80 @@ popupImageClose.addEventListener('click', () => {
     popupImage.classList.toggle('popup_show');
 });
 
-function popupProfileFormSubmitHandler (evt) {
+//----------Функция для отправки формы popupProfile
+function popupProfileFormSubmitHandler(evt) {
     //отмена стандартного поведения кнопки submit
-    evt.preventDefault(); 
+    evt.preventDefault();
     //обновляет текст в заголовке страницы. новое значение берётся из input модального окна
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     popupProfile.classList.toggle('popup_show');
 }
 
-function popupCardFormSubmitHandler (evt) {
-    evt.preventDefault(); 
-    
+//----------Функция для создания новой карточки
+function popupCardFormSubmitHandler(evt) {
+    evt.preventDefault();
+    const TITLE = popupCardForm
+        .querySelector('.popup__form-input[name=title]')
+        .value;
+
+    const LINK = popupCardForm
+        .querySelector('.popup__form-input[name=link]')
+        .value
+
+    createCard({
+        name: TITLE,
+        link: LINK, 
+    });
     popupCard.classList.toggle('popup_show');
 }
 
-function render() {
-    const HTML = CARDS
-        .map((card) => element(card))
-        .join('');
-    cards.insertAdjacentHTML('afterbegin', HTML); 
-};
+function elementRemove(event) {
+    event.target.parentElement.remove();
+}
 
-render();
+function renderTemplate(card) {
+    const TEMPLATE = document
+        .querySelector('.template')
+        .content
+        .cloneNode(true);
+    
+    let elementTitle = TEMPLATE.querySelector('.element__title');
+    let elementImage = TEMPLATE.querySelector('.element__image');
 
-let popupImageOpenAll = document.querySelectorAll('.element__image');
-popupImageOpenAll.forEach((item) => {
-    item.addEventListener('click', (event) => {
-        popupImage.classList.toggle('popup_show');
-        popupImage
-            .querySelector('.popup__figure-img')
-            .setAttribute('src', event.target.currentSrc);
-    })
-});
+    elementTitle.innerText = card.name;
+    elementImage.setAttribute('src', card.link);
+    elementImage.setAttribute('alt', `Фотография места с названием '${card.name}'.`);
 
-let treshButtonCardAll = document.querySelectorAll('.element__trash');
-treshButtonCardAll.forEach((item) => {
-    item.addEventListener('click', (event) => {
-        event.target.parentElement.remove();
-    });
-});
+    return TEMPLATE.children[0];
+}
+
+function createCard(card) {
+    let cardsSection = document.querySelector('.cards');
+ 
+    const element = renderTemplate(card);
+    cardsSection.insertAdjacentElement('afterbegin', element);
+
+    let deleteButton = element.querySelector('.element__trash')
+    deleteButton.addEventListener('click', elementRemove);
+    
+    let img = element.querySelector('.element__image');
+    img.addEventListener('click', showImg);
+
+    let like = element.querySelector('.element__like');
+    like.addEventListener('click', elementLikeActiveHandler);
+}
+
+function showImg(event) {
+    popupImage
+        .querySelector('.popup__figure-img')
+        .setAttribute('src', event.target.currentSrc);
+    popupImage.classList.toggle('popup_show');
+}
+
+function elementLikeActiveHandler(event) {
+    event.target.classList.toggle('element__like_active');
+}
+
+//----------Функция для создания новых карточек
+CARDS.forEach(createCard);
